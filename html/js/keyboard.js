1a81d847e80fb579de2fc8f054fe8d6b3ca8083e
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-07-26 12:11:25 -0300

    Set key button width

--- html/js/keyboard.js
+++ html/js/keyboard.js
@@ -1,10 +1,15 @@
 window.onload = function() {
+	var layoutWidth = 10;
+	var keyboardWidth = $('#keyboard').width();
+	var placeHolderWidth = keyboardWidth / layoutWidth;
+
 	Keyboards['pt_BR'].main.forEach(function(row, i) {
 		var keyboardRow = $('<div class="keyboard-row"/>');
 		row.forEach(function(key, x) {
 			var buttonKey = $('<button class="keyboard-key"/>');
 			buttonKey.data('key', key.key || key.label);
 			buttonKey.append($('<span>' + key.label + '</span>'));
+			buttonKey.css('width', placeHolderWidth * (key.ratio || 1));
 			keyboardRow.append(buttonKey);
 		});
 		$('#keyboard').append(keyboardRow);
@@ -25,6 +30,11 @@ window.onload = function() {
 	});*/
 
 	$('button').click(function() {
+		var key = $(this).data('key');
+
+		if (key == "Caps_Lock") {
+		}
+
 		x11.sendKey($(this).data('key'));
 	});
 }
