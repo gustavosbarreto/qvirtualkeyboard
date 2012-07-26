5917f82783ce3cb7254ca7dead77c57ead3c0766
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-07-26 09:20:52 -0300

    Add qtwebkit application loader

---
+++ src/src.pro
@@ -0,0 +1,6 @@
+QT += webkit
+
+SOURCES = main.cpp webview.cpp
+HEADERS = webview.h
+
+TARGET = tecladura
---
+++ src/webview.h
@@ -0,0 +1,14 @@
+#ifndef _WEBVIEW_H
+#define _WEBVIEW_H
+
+#include <QWebView>
+
+class WebView: public QWebView
+{
+	Q_OBJECT
+
+public:
+	WebView();
+};
+
+#endif
