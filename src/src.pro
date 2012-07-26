571379b504fe96c1b859331e1ef7911f5ce9f465
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-07-26 10:48:31 -0300

    Send keyevent to X

--- src/src.pro
+++ src/src.pro
@@ -1,4 +1,7 @@
 QT += webkit
+CONFIG += link_pkgconfig
+
+PKGCONFIG += xtst
 
 SOURCES = main.cpp x11.cpp webview.cpp
 HEADERS = x11.h webview.h
--- src/x11.cpp
+++ src/x11.cpp
@@ -1,6 +1,9 @@
 #include "x11.h"
 
+#include <QX11Info>
 #include <QDebug>
+
+#include <X11/extensions/XTest.h>
 
 X11::X11(QObject *parent): QObject(parent)
 {
@@ -8,5 +11,7 @@ X11::X11(QObject *parent): QObject(parent)
 
 void X11::sendKey(const QString &key)
 {
-	qDebug() << key;
+	int keyCode = XKeysymToKeycode(QX11Info::display(), XStringToKeysym(key.toLatin1()));
+	XTestFakeKeyEvent(QX11Info::display(), keyCode, true, CurrentTime);
+	XTestFakeKeyEvent(QX11Info::display(), keyCode, false, CurrentTime);
 }
