bf532021f5ed826190a86eaec915f2cb1b4a6aa2
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-07-26 13:59:37 -0300

    Toggle capslock state

--- html/js/keyboard.js
+++ html/js/keyboard.js
@@ -1,3 +1,9 @@
+var capsLock = false;
+
+function isSpecialKey(key) {
+	return !/^[a-z\u00C0-\u00ff]+$/.test(key);
+}
+
 window.onload = function() {
 	var layoutWidth = 10;
 	var keyboardWidth = $('#keyboard').width();
@@ -6,7 +12,7 @@ window.onload = function() {
 	Keyboards['pt_BR'].main.forEach(function(row, i) {
 		var keyboardRow = $('<div class="keyboard-row"/>');
 		row.forEach(function(key, x) {
-			var buttonKey = $('<button class="keyboard-key"/>');
+			var buttonKey = $('<button class="keyboard-key' + (!isSpecialKey(key.key) ? ' alphabetical-key' : '') + '"/>');
 			buttonKey.data('key', key.key || key.label);
 			buttonKey.append($('<span>' + key.label + '</span>'));
 			buttonKey.css('width', placeHolderWidth * (key.ratio || 1));
@@ -18,8 +24,8 @@ window.onload = function() {
 	$('button').click(function() {
 		var key = $(this).data('key');
 
-		if (key == "Caps_Lock") {
-		}
+		if (key == "Caps_Lock")
+			$('#keyboard').find('button.alphabetical-key').find('span').css('text-transform', ((capsLock = !capsLock) ? 'uppercase' : 'lowercase'));
 
 		x11.sendKey($(this).data('key'));
 	});
