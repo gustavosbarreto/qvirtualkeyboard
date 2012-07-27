03d836d0e72a485f548f7b8f89a014258a4c8fd1
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-07-27 11:48:48 -0300

    Show window popup

--- src/webview.cpp
+++ src/webview.cpp
@@ -3,11 +3,38 @@
 
 #include <QWebFrame>
 
-WebView::WebView(): QWebView()
+WebView::WebView(QWidget *parent): QWebView(parent)
 {
 	setAttribute(Qt::WA_X11DoNotAcceptFocus);
 	setFocusPolicy(Qt::NoFocus);
+
+	setPage(new WebPage(this));
+
+	settings()->setAttribute(QWebSettings::JavascriptEnabled, true);
+	settings()->setAttribute(QWebSettings::JavascriptCanOpenWindows, true);
+
 	load(QUrl("qrc:///html/index.html"));
 	page()->mainFrame()->addToJavaScriptWindowObject("x11", new X11(this));
 }
 
+WebPage::WebPage(QObject *parent, bool popup): QWebPage(parent)
+{
+	if (popup)
+	{
+		WebView *webView = new WebView;
+		webView->setAttribute(Qt::WA_DeleteOnClose);
+		webView->setPage(this);
+		webView->show();
+		connect(webView->page(), SIGNAL(windowCloseRequested()), SLOT(closeWebView()));
+	}
+}
+
+void WebPage::closeWebView() {
+	qobject_cast<QWebView *>(view())->close();
+}
+
+QWebPage *WebPage::createWindow(QWebPage::WebWindowType type)
+{
+	QWebPage::createWindow(type);
+	return new WebPage(this, true);
+}
--- src/webview.h
+++ src/webview.h
@@ -8,7 +8,21 @@ class WebView: public QWebView
 	Q_OBJECT
 
 public:
-	WebView();
+	WebView(QWidget *parent = 0);
+};
+
+class WebPage: public QWebPage
+{
+	Q_OBJECT
+
+public:
+	WebPage(QObject *parent, bool popup = false);
+
+private slots:
+	void closeWebView();
+
+protected:
+	QWebPage *createWindow(QWebPage::WebWindowType type);
 };
 
 #endif
