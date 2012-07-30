96e8dc1f8bad3cd4cecdb2e504cad53bcebf422a
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2012-07-30 14:03:54 -0300

    Set keyboard position

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QDesktopWidget>
+#include <QDebug>
 
 #include "webview.h"
 
@@ -6,8 +8,13 @@ int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
 
 	WebView *view = new WebView(NULL);
+
+	QDesktopWidget *desktop = QApplication::desktop();
+	QRect rect = desktop->screenGeometry();
+
 	view->show();
-	view->move(200, 400);
+	view->setFixedSize((rect.width() / 100) * 50, (rect.height() / 100) * 35); 
+	view->move((rect.width() - view->width()) / 2, rect.height() - view->height());
 
 	return app.exec();
 }
